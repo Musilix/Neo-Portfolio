0780c4fa2d5ec451fc08f517a9b81f4d1d0842bb
Kareem <kshehab1@outlook.com>
2020-09-26 20:49:31 -0400

    Laying out foundation for splash page. Nav bar added + stylized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// routable components
+import { SplashPageComponent } from './splash-page/splash-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SplashPageComponent},
+  // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
