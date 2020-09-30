8f82dd6709dcbdafb059561e1e1bd1cd217604f1
Kareem <kshehab1@outlook.com>
2020-09-30 00:31:00 -0400

    Trying to fix how wakatime  API is called

--- src/app/http-stats-interceptor.ts
+++ src/app/http-stats-interceptor.ts
@@ -17,6 +17,7 @@ export class HttpStatsInterceptor implements HttpInterceptor{
     constructor(){}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
+        console.log("REQ URL: " + request.url);
         if(request.url.indexOf("https://musilix.github.io/Neo-Portfolio/") > 0){
             console.log("Prod");
 
