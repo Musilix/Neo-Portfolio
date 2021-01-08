950f12536a56e944c35acec55dea41ae8300810d
Kareem Shehab <kshehab1@outlook.com>
2021-01-07 20:07:06 -0500

    Finished left side nav logic. Need to finish styling now

--- src/app/extras-page/extras-page.component.ts
+++ src/app/extras-page/extras-page.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./extras-page.component.css']
 })
 export class ExtrasPageComponent implements OnInit {
+  private tabMappings = {0: "endeavours", 1: "art", 2: "hobbies"};
+  public activeTabIdx = 0;
+  // private activeTab = this.tabMappings[this.activeTabIdx];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  scrollTo(sectionIdx){
+    this.activeTabIdx = sectionIdx;
+
+    let scrollTo : HTMLElement = document.getElementById(this.tabMappings[this.activeTabIdx]);
+    scrollTo.scrollIntoView({behavior: "smooth", block: "end"});
   }
 
 }
