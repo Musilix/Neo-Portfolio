a945442ce4f9d8e3798e9b2d114708bc2eae47d4
Kareem Shehab <kshehab1@outlook.com>
2021-01-10 17:59:11 -0500

    Finally finished logic for sidebar nav on extras pg

--- src/app/extras-page/extras-page.component.ts
+++ src/app/extras-page/extras-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ArtService } from '../art.service';
 
 @Component({
@@ -7,31 +7,91 @@ import { ArtService } from '../art.service';
   styleUrls: ['./extras-page.component.css']
 })
 export class ExtrasPageComponent implements OnInit {
-  private tabMappings = {0: "endeavours", 1: "art", 2: "hobbies"};
+  // complicated way
+  private idxToTabMappings = {0: "endeavours", 1: "art", 2: "hobbies"};
+  private tabToIdxMappings = {"endeavours": 0, "art": 1, "hobbies": 2};
+
   public activeTabIdx = 0;
 
+  private tabsToListenTo: HTMLElement[] = [];
+  private scrollRef: HTMLElement = document.getElementsByTagName("body")[0];
+
+  private buttonPressed: boolean = false;
+  private scrollListener;
+
+  private scrollTimeout;
+  
   constructor(private artService: ArtService) { }
 
   ngOnInit(): void {
   }
 
-  // function for scrolling to specific section, depending on idx given + mapping value
-  scrollTo(sectionIdx){
-    this.activeTabIdx = sectionIdx;
+  ngAfterViewInit(){
+    // set up tabsToWatch... we will loop thru this list of tabs whenever the user scrolls and check to see which ele is in view
+    // seems like a pretty... bad way to check for this, but seeing as there is only 3 tabs to check each time, should be fine for now...
+    for(let i = 0; i < Object.keys(this.idxToTabMappings).length; i++){
+      this.tabsToListenTo.push(document.getElementById(this.idxToTabMappings[i]));
+    }
 
-    let scrollTo : HTMLElement = document.getElementById(this.tabMappings[this.activeTabIdx]);
-    scrollTo.scrollIntoView({behavior: "smooth", block: "end"});
+    // modularize scroll logic so we can easily use it or pause it with the event listeners.
+    this.scrollListener = () => {
+      //if the button was pressed, we should pause the logic for tab finding... and simply just call the function which intiailizes our timeout for determing when the buttonPress scroll has stopped
+      if(this.buttonPressed){
+        this.checkIfEnded();
+      }else{
+        //loop thru the tab elements, get their bounding box, and then implement logic to determine if the tab is in the window/document viewport
+        for(let i = 0; i < this.tabsToListenTo.length; i++){
+          let currItem = this.tabsToListenTo[i].getBoundingClientRect();
+
+          // logic to check if a tab/section is in the viewport, give or take 20px for the nav bar on top...
+          // although, I know that nav bar is NOT 20px... so -\(._.)/-
+          if(Math.floor(currItem.top + 50) >= 0 && Math.floor(currItem.bottom - 50) <= (window.innerHeight || document.documentElement.clientHeight) ){
+            this.activeTabIdx = this.tabToIdxMappings[this.idxToTabMappings[i]];
+          }
+        }
+      }
+    }
+
+    // initialization of event listener on component init
+    this.scrollRef.addEventListener("scroll", this.scrollListener);
+  }
+
+  /* this function will be called if the buttonPressed bool has been set to true(which should only happen on explicit click of a side tab button)
+   it will set a timeout each time, which will come to be overrided if the window is still scrolling... if the window is no longer scrolling, the timeout function
+   will have time to execute, where it will be able to set buttonPressed bool to false
+  */
+  checkIfEnded(){
+    if(this.scrollTimeout){
+      clearTimeout(this.scrollTimeout);
+    }
+
+    /* HOLY FUCKING FUCK, IT TOOK ME 2 DAYS TO REALIZE THAT THE REASON
+       THIS TIMEOUT CALLBACK FUNCTION WASNT UPDATING THE GLOBAL SCOPED "BUTTONPRESSED"
+       VAL WAS BCUZ THE LEXICAL SCOPE WAS COMPLETELY DIFFERENT WHEN NOT USING
+       AND ANON FUNCTION AS THE CALLBACK... WOW... lesson learned I guess
+    */
+    this.scrollTimeout = setTimeout(() => {
+        this.buttonPressed = false;
+    }, 100);
+  }
+
+  // function for scrolling to specific section, depending on idx given + mapping value
+  scrollTo(sectionIdx){  
+    this.activeTabIdx = sectionIdx;
+    this.buttonPressed = true;
+
+    let scrollToEle : HTMLElement = document.getElementById(this.idxToTabMappings[this.activeTabIdx]);
+
+    scrollToEle.scrollIntoView({behavior: "smooth", block: "start"});
   }
 
   // TODO: move this from teh extras component into the svc that manages art...
   // the art svc, that is!
   displayArt(idx){
     this.artService.displayProj(idx);
-    // console.log(artBio);
   }
 
   getArt(){
     return this.artService.getArt();
   }
-
 }
