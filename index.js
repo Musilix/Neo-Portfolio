6bf5f408e99e0cab981485478da54744038c8fd9
Kareem <kshehab1@outlook.com>
2021-04-19 00:37:27 -0400

    1 more commit and i will pull my hair out until im bald

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const app = express();
 const path = require('path');
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/docs/portfolio-website'));
+app.use(express.static(__dirname + '/docs'));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/docs/portfolio-website/index.html"));
+  res.sendFile(path.join(__dirname + "/docs/index.html"));
 });
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + "/docs/portfolio-website/index.html"));
+    res.sendFile(path.join(__dirname + "/docs/index.html"));
   });
 
 // Start the app by listening on the default Heroku port
