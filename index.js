d2e1ca8c3e347bbd826fafb1a4296c97c6dca071
Kareem <kshehab1@outlook.com>
2021-04-20 22:08:54 -0400

    Burn out, incoming!!!

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/docs'));
-app.use(requireHTTPS);
+// app.use(requireHTTPS);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", 'https://www.kareemshehab.com');
@@ -22,10 +22,10 @@ app.get("/", function (req, res) {
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5000);
 
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
+// function requireHTTPS(req, res, next) {
+//   // The 'x-forwarded-proto' check is for Heroku
+//   if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//       return res.redirect('https://' + req.get('host') + req.url);
+//   }
+//   next();
+// }
