78eeb7b1df383512c7c494a5f6f486503bb1952c
Kareem <kshehab1@outlook.com>
2021-04-28 22:07:24 -0400

    Oregon was fun. Trying to fix some junks

--- index.js
+++ index.js
@@ -3,24 +3,29 @@ const app = express();
 
 const path = require("path");
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/docs'));
-// app.use(requireHTTPS);
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", 'https://www.kareemshehab.com');
 });
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/docs/index.html"));
-});
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/docs'));
+
+app.use(cors({
+  origin: 'https://www.kareemshehab.com'
+}));
+
+// app.use(requireHTTPS);
+
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname + "/docs/index.html"));
+// });
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/docs/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 4200);
 
 // function requireHTTPS(req, res, next) {
 //   // The 'x-forwarded-proto' check is for Heroku
