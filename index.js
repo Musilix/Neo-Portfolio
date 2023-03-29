6e57d89b12e9c8bb5b088b7cbb4d60623e6259e1
Kareem Shehab <kshehab1@outlook.com>
2023-03-28 18:42:26 -0700

    Trying to enforce HTTPS when a user tries to go the HTTP ver of site

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const app = express();
 
 const path = require("path");
 const cors = require("cors");
-const fetch = require('node-fetch');
-const nodemailer = require('nodemailer');
+const fetch = require("node-fetch");
+const nodemailer = require("nodemailer");
+const enforce = require("express-sslify");
 
-const ENV_DIR = process.env.NODE_ENV === "production" ? "/docs" : "/src"
+const ENV_DIR = process.env.NODE_ENV === "production" ? "/docs" : "/src";
 
 //REMOVE?
-app.enable('trust proxy');
-app.disable('strict routing');
+app.enable("trust proxy");
+app.disable("strict routing");
+
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.use(express.json());
 app.use(cors());
 // app.use(enforce.HTTPS());
-app.use(express.static(__dirname + '/docs'));
+app.use(express.static(__dirname + "/docs"));
 
 const routes = ["/", "/about", "/projects", "/contact", "/extras"];
 
@@ -28,29 +31,31 @@ const routes = ["/", "/about", "/projects", "/contact", "/extras"];
 //   }
 // });
 
-routes.forEach(route => {
+routes.forEach((route) => {
   app.get(route, (req, res) => {
     res.sendFile(path.join(__dirname, `docs/index.html`));
   });
 });
 
-app.get("/stats", async(req, res) => {
-  const waka_url = "https://wakatime.com/api/v1/users/current/stats/last_7_days?api_key=" + process.env.WAKA_API_KEY;
-  const waka_res = await fetch(waka_url)
+app.get("/stats", async (req, res) => {
+  const waka_url =
+    "https://wakatime.com/api/v1/users/current/stats/last_7_days?api_key=" +
+    process.env.WAKA_API_KEY;
+  const waka_res = await fetch(waka_url);
   const waka_data = await waka_res.json();
 
   res.send(waka_data);
 });
 
-app.post("/sendmail", async(req, res) => {
+app.post("/sendmail", async (req, res) => {
   console.log("Sending Email: ");
   console.log(req.body);
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    service: "Gmail",
     auth: {
       user: process.env.EMAIL_USER_RUNT,
-      pass: process.env.EMAIL_PASS
-    }
+      pass: process.env.EMAIL_PASS,
+    },
   });
 
   const mailOptions = {
@@ -58,15 +63,15 @@ app.post("/sendmail", async(req, res) => {
     to: process.env.EMAIL_USER,
     secure: true,
     subject: req.body.topic,
-    text: `Email from ${req.body.emailAddress}: \n ${req.body.message}`
+    text: `Email from ${req.body.emailAddress}: \n ${req.body.message}`,
   };
 
   try {
     let info = await transporter.sendMail(mailOptions);
-    console.log('Email sent: ' + info.response);
+    console.log("Email sent: " + info.response);
     let sendStatus = true;
     res.send(sendStatus);
-  }catch(e){
+  } catch (e) {
     console.error(`Something went dreadfully wrong: ${e}`);
     let sendStatus = false;
     res.send(sendStatus);
@@ -74,6 +79,6 @@ app.post("/sendmail", async(req, res) => {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200, ()=> {
+app.listen(process.env.PORT || 4200, () => {
   console.log(`app started on ${process.env.PORT || 4200}, bruh`);
 });
