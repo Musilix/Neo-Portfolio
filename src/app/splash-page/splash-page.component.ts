f6cbee837d57b5f54db1ec3bb2d130338537a389
Kareem Shehab <kshehab1@outlook.com>
2024-04-23 23:41:33 -0700

    setting up spline for 3d stuff

--- src/app/splash-page/splash-page.component.ts
+++ src/app/splash-page/splash-page.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-splash-page',
-  templateUrl: './splash-page.component.html',
-  styleUrls: ['./splash-page.component.css']
+  selector: "app-splash-page",
+  templateUrl: "./splash-page.component.html",
+  styleUrls: ["./splash-page.component.css"],
 })
 export class SplashPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
